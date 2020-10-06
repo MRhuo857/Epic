5d2ce7503019391857568d975ff0070d8f9fe929
mr.huo <455801665@qq.com>
2020-10-06 14:29:03 +0800

    完成路由

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from "react-router-dom";
+function Header(){
+  return (
+    <div>
+      <nav>
+        <Link to="/">首页</Link>
+        <Link to="history">上传历史</Link>
+        <Link to="/about">关于我</Link>
+      </nav>
+    </div>
+  )
+}
+export default Header
