c4269fd462905cf4665a040208e19dd39df4e624
mr.huo <455801665@qq.com>
2020-11-15 13:13:48 +0800

    设置欢迎语

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
-function Home(){
-  return (
-    <>
-      <h1>Home</h1>
-      </>
+import {observer} from "mobx-react";
+import {useStores} from "../stores";
+
+const Home=observer(()=>{
+  const {UserStore}=useStores()
+  return(
+    <div>
+      {
+        UserStore.currentUser ?
+          <>
+            HELLO {UserStore.currentUser.attributes.username}，欢迎登录！
+          </> :
+          <>
+            请先登录！
+          </>
+      }
+    </div>
   )
-}
+})
 export default Home;
