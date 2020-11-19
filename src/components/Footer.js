0765accd8940d9dbb8795fdfee49a041843756c0
mr.huo <455801665@qq.com>
2020-11-19 22:02:50 +0800

    关于页面

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,15 @@ padding:10px 100px;
 text-align:center;
 color:#aaa;
 font-size:12px;
+background: #eee;
 `
 
 function Component(){
   return (
     <Footer>
-      <p>Footer</p>
+      <div>
+        请勿上传违反中国大陆和香港法律的图片，违者后果自负。
+      </div>
     </Footer>
   )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,36 @@
 import React from "react";
-function About(){
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 0 auto;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+`
+const Section = styled.section`
+  margin-top: 20px;
+  border: 1px solid #eee;
+  padding: 10px 10px;
+`
+
+function About() {
   return (
     <>
-      <h1>About</h1>
+      <Title>关于本图床</Title>
+      <Section>
+        本图床为个人项目，并保留随时删除图片并举报上传违规图片者的权利。
+      </Section>
+      <Section>
+        <h2>严禁上传及分享如下类型的图片：</h2>
+        <ol>
+          <li>含有色情、暴力、宣扬恐怖主义的图片</li>
+          <li>侵犯版权、未经授权的图片</li>
+          <li>其他违反中华人民共和国法律的图片</li>
+          <li>其他违反香港法律的图片</li>
+        </ol>
+      </Section>
     </>
   )
 }
+
 export default About
